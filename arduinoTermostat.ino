47075252618ca48bd4a489e0fd755ea868e7d5ad
Lars Wattsgard <lawa@wsi.nu>
2018-11-09 23:18:12 +0100

    reading the temp from the sensor, still no logic in place

--- arduinoTermostat.ino
+++ arduinoTermostat.ino
@@ -1,3 +1,20 @@
+// First we include the libraries
+#include <OneWire.h> 
+#include <DallasTemperature.h>
+
+/********************************************************************/
+// Data wire is plugged into pin 2 on the Arduino 
+#define ONE_WIRE_BUS 2 
+/********************************************************************/
+// Setup a oneWire instance to communicate with any OneWire devices  
+// (not just Maxim/Dallas temperature ICs) 
+OneWire oneWire(ONE_WIRE_BUS); 
+/********************************************************************/
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+/********************************************************************/ 
+
+
 int LEDPin = 12;
 int inPin = 11;
 int buttonPressed = 0;
@@ -9,6 +26,9 @@ void setup() {
   pinMode(LEDPin, OUTPUT);
   pinMode(inPin, INPUT);
   Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo"); 
+  // Start up the library 
+  sensors.begin(); 
 
 }
 
@@ -28,6 +48,21 @@ void loop() {
     //If it was pressed, then toggle the LEDs each 500 ms
     //blinkLoop(500);
   }
+
+   // call sensors.requestTemperatures() to issue a global temperature 
+   // request to all devices on the bus 
+  /********************************************************************/
+   Serial.print(" Requesting temperatures..."); 
+   sensors.requestTemperatures(); // Send the command to get temperature readings 
+   Serial.println("DONE"); 
+  /********************************************************************/
+   Serial.print("Temperature is: "); 
+   Serial.print(sensors.getTempCByIndex(0)); // Why "byIndex"?  
+     // You can have more than one DS18B20 on the same bus.  
+     // 0 refers to the first IC on the wire 
+     delay(1000); 
+
+  
 
 
 }
